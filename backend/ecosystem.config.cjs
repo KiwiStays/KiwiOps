360b200354aa8063431b985a8b188af9c45e9a45
KiwiStays <bookings@kiwistays.in>
2025-03-14 03:21:40 +0530

    Update ecosystem.config.cjs

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
         script: "src/index.js",
         env: {
             MONGO_URI: "mongodb+srv://bookings:bookings123@cluster0.7ar1u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-            PORT : 3000,
+            PORT : 4000,
             CLOUDINARY_API_SECRET: "qsL8Ro_eg4FSzlH4EG41HYrACWc",
             CLOUDINARY_API_KEY :  "834171772782952",
             CLOUDINARY_CLOUD_NAME : "dghy5vp6u",
